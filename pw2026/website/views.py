ace9261e9398ca132cfb3dc6af8186111aa0db59
Professor <rafael.zottesso@ifpr.edu.br>
2026-03-05 09:19:32 -0300

    Criação de outras páginas, urls e templates

--- pw2026/website/views.py
+++ pw2026/website/views.py
@@ -3,4 +3,8 @@ from django.views.generic import TemplateView
 class Index(TemplateView):
     template_name = "website/inicio.html"
 
-    
+class Sobre(TemplateView):
+    template_name = "website/sobre.html"
+
+class Contato(TemplateView):
+    template_name = "website/contato.html"    
