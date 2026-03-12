5e63a6e3f14d1dde4cb72a7ea42a798c2ecdd0f0
AndreyCubas <felipedreycubascardoso@gmail.com>
2026-03-12 09:12:22 -0300

    feat: likando com o modelo.html

--- pw2026/website/urls.py
+++ pw2026/website/urls.py
@@ -4,9 +4,10 @@ from .views import Index, Sobre, Contato, Login, Cadastro
 urlpatterns = [
     # path("admin/", admin.site.urls),
 
-    path("inicio/", Index.as_view(), name="pagina_inicial"),
+    path("inicio/", Index.as_view(), name="inicio"),
     path("sobre/", Sobre.as_view(), name="sobre"),
     path("contato/", Contato.as_view(), name="contato"),
     path("login/", Login.as_view(), name="login"),
     path("cadastro/", Cadastro.as_view(), name="cadastro"),
+
 ]
